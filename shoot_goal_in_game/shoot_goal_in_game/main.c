75dcf08a668114cf9b2e509c9931d57adbe5a112
khyun98 <khyun9807@naver.com>
2023-07-03 13:08:02 +0900

    change pos var name

--- shoot_goal_in_game/shoot_goal_in_game/main.c
+++ shoot_goal_in_game/shoot_goal_in_game/main.c
@@ -5,16 +5,16 @@
 #include "Screen.h"
 
 PLAYER g_sPlayer;
-char g_strPlayer[] = "┗━━●━━┛";
+char g_strPlayer[] = "┗━●━┛";
 int g_nLength;
 
 void Init() {
 	g_nLength = strlen(g_strPlayer);
 	g_sPlayer.nCenterX = (g_nLength+1)/2;
 	g_sPlayer.nCenterX = 0;
-	g_sPlayer.nMoveX = 20;
-	g_sPlayer.nMoveY = 22;
-	g_sPlayer.nX = g_sPlayer.nMoveX - g_sPlayer.nCenterX;
+	g_sPlayer.nPosX = 20;
+	g_sPlayer.nPosY = 22;
+	g_sPlayer.nX = g_sPlayer.nPosX - g_sPlayer.nCenterX;
 }
 
 void Update() {}
@@ -32,14 +32,27 @@ int main() {
 	Init();
 
 	while (1) {
-		if (_kbhit()) {
+		if (_kbhit()) {//키보드
 			nKey = _getch();
 			if (nKey == 'q')
 				break;
 			switch (nKey) {
 			case 'j':
+				g_sPlayer.nPosX--;//이동
+				if (g_sPlayer.nPosX - g_sPlayer.nCenterX < 0)//좌접
+					g_sPlayer.nPosX--;
+				//특수기호는 2바이트이므로 1바이트만 잘릴 수 없다.
+
+				g_sPlayer.nX = g_sPlayer.nPosX - g_sPlayer.nCenterX;
 				break;
 			case 'l':
+				g_sPlayer.nPosX++;//이동
+				nRemain = g_nLength - (g_sPlayer.nCenterX + 1);
+				//중심좌표로 부터 캐릭터 크기. 우측경계접촉 확인용
+				if (g_sPlayer.nPosX + nRemain > 79)//우접
+					g_sPlayer.nPosX++;//1바이트 더 옮겨줌
+
+				g_sPlayer.nX = g_sPlayer.nPosX - g_sPlayer.nCenterX;
 				break;
 			case 'k':
 				break;
--- shoot_goal_in_game/shoot_goal_in_game/solid.h
+++ shoot_goal_in_game/shoot_goal_in_game/solid.h
@@ -2,6 +2,6 @@
 
 typedef struct _PLAYER {
 	int nCenterX, nCenterY;//중심좌표
-	int nMoveX, nMoveY;//이동좌표
-	int nX, nY;//
+	int nPosX, nPosY;//실제위치좌표
+	int nX, nY;//캐릭터 출력 시작 좌표
 }PLAYER;
