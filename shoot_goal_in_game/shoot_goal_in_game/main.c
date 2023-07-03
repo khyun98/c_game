4eb5db8fe18b9f38cd065b245ad18bf4b2cc8cfc
khyun98 <khyun9807@naver.com>
2023-07-03 15:01:50 +0900

    done with player movement

--- shoot_goal_in_game/shoot_goal_in_game/main.c
+++ shoot_goal_in_game/shoot_goal_in_game/main.c
@@ -11,7 +11,7 @@ int g_nLength;
 void Init() {
 	g_nLength = strlen(g_strPlayer);
 	g_sPlayer.nCenterX = (g_nLength+1)/2;
-	g_sPlayer.nCenterX = 0;
+	g_sPlayer.nCenterY = 0;
 	g_sPlayer.nPosX = 20;
 	g_sPlayer.nPosY = 22;
 	g_sPlayer.nX = g_sPlayer.nPosX - g_sPlayer.nCenterX;
@@ -20,7 +20,26 @@ void Init() {
 void Update() {}
 
 void Render() {
+	char tempBuffer[100] = { 0, };
+	ScreenClear();
 	
+
+	if (g_sPlayer.nX < 0) {
+		ScreenPrint(0, g_sPlayer.nPosY, &g_strPlayer[-1 * g_sPlayer.nX]);
+	}
+	else if (g_sPlayer.nX + g_nLength > 79) {
+		strncpy_s(tempBuffer, sizeof(tempBuffer), g_strPlayer, g_nLength - (g_sPlayer.nX + g_nLength - 79));
+		ScreenPrint(g_sPlayer.nX, g_sPlayer.nPosY, tempBuffer);
+	}
+	else if(g_sPlayer.nX>=0&&g_sPlayer.nX<=79){
+		ScreenPrint(g_sPlayer.nX,g_sPlayer.nPosY,g_strPlayer);
+	}
+	memset(tempBuffer, 0, sizeof(tempBuffer));
+	sprintf_s(tempBuffer, sizeof(tempBuffer),"player position x:%d y:%d", g_sPlayer.nPosX, g_sPlayer.nPosY);
+	ScreenPrint(0, 0, tempBuffer);
+	
+
+	ScreenFlipping();
 }
 
 void Release() {}
@@ -38,18 +57,23 @@ int main() {
 				break;
 			switch (nKey) {
 			case 'j':
+				if (g_sPlayer.nPosX <= 1)
+					break;
 				g_sPlayer.nPosX--;//이동
-				if (g_sPlayer.nPosX - g_sPlayer.nCenterX < 0)//좌접
+				nRemain = g_nLength - (g_sPlayer.nCenterX + 1);
+				if (g_sPlayer.nPosX + nRemain > 79 || g_sPlayer.nPosX - g_sPlayer.nCenterX < 0)//좌접
 					g_sPlayer.nPosX--;
 				//특수기호는 2바이트이므로 1바이트만 잘릴 수 없다.
 
 				g_sPlayer.nX = g_sPlayer.nPosX - g_sPlayer.nCenterX;
 				break;
 			case 'l':
+				if (g_sPlayer.nPosX >= 77)
+					break;
 				g_sPlayer.nPosX++;//이동
 				nRemain = g_nLength - (g_sPlayer.nCenterX + 1);
 				//중심좌표로 부터 캐릭터 크기. 우측경계접촉 확인용
-				if (g_sPlayer.nPosX + nRemain > 79)//우접
+				if (g_sPlayer.nPosX + nRemain > 79|| g_sPlayer.nPosX - g_sPlayer.nCenterX < 0)//우접
 					g_sPlayer.nPosX++;//1바이트 더 옮겨줌
 
 				g_sPlayer.nX = g_sPlayer.nPosX - g_sPlayer.nCenterX;
@@ -58,7 +82,10 @@ int main() {
 				break;
 			}
 		}
+		Update();
+		Render();
 	}
+
 
 	Release();
 	ScreenRelease();
