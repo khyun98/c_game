d2ee3c13fbf7dfcca72d616ec1a2b194f3d10f40
khyun98 <khyun9807@naver.com>
2023-07-11 17:56:04 +0900

    basic structure

--- catch_mole_game/catch_mole_game/main.c
+++ catch_mole_game/catch_mole_game/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
+void Init();
+
+void Update();
+
+void Render();
+
+void Release();
+
 int main() {
+	Init();
+
+	while (1) {
 
 
+		Update();
+		Render();
+	}
 
+	Release();
 	return 0;
 }
+
+void Init() {}
+
+void Update() {}
+
+void Render() {}
+
+void Release() {}
